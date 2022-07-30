f780ece8a89863b2c715f4b214a80df4e4d6c36b
Drew Keat <drewjkeat@gmail.com>
2022-07-30 20:28:25 +0000

    add mui

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import {Button} from "@mui/material"
 
 const Header=(props)=>{
   return(
     <>
       <h1>Test Header</h1>
+      <Button variant="contained" primary>Click Me</Button>
     </>
   )
 }
