7db76262f364798838cd3cba5e6f4b743101804f
Drew Keat <drewjkeat@gmail.com>
2022-07-31 20:55:50 +0000

    Clean up unncessary files, test update from cs - on computer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import {Button} from "@mui/material"
 const Header=(props)=>{
   return(
     <>
-      <h1>Test Header</h1>
+      <h1>Updated Header</h1>
       <Button variant="contained" primary>Click Me</Button>
     </>
   )
