e16419accb0f258650ac09f0248706509f788a8e
drewkeat <drewjkeat@gmail.com>
2022-08-01 16:33:55 -0600

    add boxes for grid items on landing page

--- src/Containers/Landing.jsx
+++ src/Containers/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 import { Header } from "../Components";
 
@@ -7,21 +7,31 @@ export default function Landing() {
   return (
     <Grid
       container
-      justifyContent={"center"}
       justifyItems={"center"}
       id="main-wrapper"
+      columns={{ xs: 4, sm: 8, md: 12 }}
     >
-      <Grid container xs={12} justifyContent="center" alignContent="start">
+      {/* Row 1 */}
+      <Grid item component={Box} xs={2} backgroundColor={"secondary.light"} />
+      <Grid item component={Box} xs={2} backgroundColor={"primary.dark"} />
+      <Grid container xs={6} justifyContent="center" alignContent="start">
         <Header />
       </Grid>
+      <Grid item component={Box} xs={2} backgroundColor={"secondary.main"} />
+      {/* row 2 */}
+      <Grid item component={Box} xs={2} backgroundColor={"primary.light"} />
       <Grid item xs={8}>
         <Typography variant="h3">Main Content Area</Typography>
       </Grid>
-      <Grid container xs={12} justifyContent="center" alignContent="end">
+      <Grid item component={Box} xs={2} backgroundColor={"secondary.dark"} />
+      {/* row 3 */}
+      <Grid item component={Box} xs={2} backgroundColor={"secondary.main"} />
+      <Grid container xs={6} justifyContent="center" alignContent="end">
         <Typography variant="h6" textAlign="center">
           Copyright info
         </Typography>
       </Grid>
+      <Grid item component={Box} xs={4} backgroundColor={"success.light"} />
     </Grid>
   );
 }
