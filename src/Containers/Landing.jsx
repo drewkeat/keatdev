cd38195242cf8137868f71fa7d7722eb9f6ff2c7
Drew Keat <drewjkeat@gmail.com>
2022-08-16 18:56:10 +0000

    Add Grid Items to Welcome Text

--- src/Containers/Landing.jsx
+++ src/Containers/Landing.jsx
@@ -8,8 +8,12 @@ export default function Landing() {
   return (
     <Container disableGutters="true" maxWidth="xl" sx={{height: "100%", backgroundColor: "primary.dark"}}>
       <Grid container justifyContent="center" alignContent={'center'} height='100%'>
-        <Typography variant="h3" textAlign="center">Welcome to <span class="headline-emphasized">KeatDev</span></Typography>
-        <Typography variant="h3" textAlign="center">I'm <span class="headline-emphasized">Drew Keat</span></Typography>
+        <Grid item xs={12}>
+          <Typography variant="h3" textAlign="center">Welcome to <span class="headline-emphasized">KeatDev</span></Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h3" textAlign="center">I'm <span class="headline-emphasized">Drew Keat</span></Typography>
+        </Grid>
       </Grid>
     </Container>
   );
